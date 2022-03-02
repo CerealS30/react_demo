9ba2be241d40d2f750e69cca9ee0d1cb11d47cc1
Miguel <m_herreraislas@hotmail.com>
2022-03-02 11:09:26 -0600

    add mul component Closes #6

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Demo calculator
+          Demo calculator - by Miguel
         </p>
 
 	  <Addition />
